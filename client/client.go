a5c5f8f1e6396eea84b299c2a54c957716e3b7f2
ablequan <chenhqchn@gmail.com>
2022-01-09 18:57:26 +0800

    修复获取构建规则列表bug

--- client/client.go
+++ client/client.go
@@ -3,7 +3,6 @@
 package client
 
 import (
-	"fmt"
 	openapi "github.com/alibabacloud-go/darabonba-openapi/client"
 	endpointutil "github.com/alibabacloud-go/endpoint-util/service"
 	openapiutil "github.com/alibabacloud-go/openapi-util/service"
@@ -2231,10 +2230,6 @@ func (client *Client) GetRepoBuildRuleListWithOptions(RepoNamespace *string, Rep
 	if _err != nil {
 		return _result, _err
 	}
-
-	fmt.Println("--------")
-	fmt.Println(_body)
-	fmt.Println("--------")
 
 	_err = tea.Convert(_body, &_result)
 	return _result, _err
